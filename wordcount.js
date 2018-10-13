536e00380655b95e7fa1052928fac80e10ca0e34
RaahelBa1g <raahelbaig58@gmail.com>
2018-10-13 16:04:55 +0530

    Equations corrected

--- wordcount.js
+++ wordcount.js
@@ -4,21 +4,26 @@
     console.log("Entered funtion");
     var val = document.getElementById("spaces").value;
     var wordcount =0;
+    var spacecount = 0;
     var sentcount =0;
     for(var i=0;i<val.length;i++){
         console.log("Entered for");
         if(val[i]==" "){
-            wordcount++;
+            if(val[i+1]!=" ")
+            {
+                wordcount++;
+            }
+            spacecount++;
         }
         if(val[i]=="."){
             sentcount++;
         }
    }
     
-    document.getElementById("space").innerHTML = wordcount;
-    document.getElementById("chars").innerHTML = val.length-wordcount;
+    document.getElementById("space").innerHTML = spacecount;
+    document.getElementById("chars").innerHTML = val.length-spacecount-sentcount;
     document.getElementById("words").innerHTML = wordcount+1;
     document.getElementById("sentences").innerHTML = sentcount;
-    console.log(count+1);
+   // console.log(count+1); commented this out because error shows up in the console
     };
     
