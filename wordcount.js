e94ce4ae84468e9a53119ded9e6264ed018a9d31
over-engineer <dev@over-engineer.com>
2019-10-27 03:42:19 +0300

    Fix indentation and consistent whitespace

--- wordcount.js
+++ wordcount.js
@@ -1,29 +1,31 @@
-    var mouse = document.getElementById("func");
+var mouse = document.getElementById("func");
     
-    mouse.onclick=function(){
+mouse.onclick = function() {
     console.log("Entered funtion");
+    
     var val = document.getElementById("spaces").value;
     var wordcount =0;
     var spacecount = 0;
     var sentcount =0;
-    for(var i=0;i<val.length;i++){
+    
+    for (var i = 0; i < val.length; i++) {
         console.log("Entered for");
-        if(val[i]==" "){
-            if(val[i+1]!=" ")
-            {
+        
+        if (val[i] == " ") {
+            if (val[i + 1] != " ") {
                 wordcount++;
             }
             spacecount++;
         }
-        if(val[i]=="."){
+        
+        if (val[i] == ".") {
             sentcount++;
         }
    }
     
     document.getElementById("space").innerHTML = spacecount;
-    document.getElementById("chars").innerHTML = val.length-spacecount-sentcount;
-    document.getElementById("words").innerHTML = wordcount+1;
+    document.getElementById("chars").innerHTML = val.length - spacecount - sentcount;
+    document.getElementById("words").innerHTML = wordcount + 1;
     document.getElementById("sentences").innerHTML = sentcount;
-   // console.log(count+1); commented this out because error shows up in the console
-    };
-    
+    // console.log(count+1); commented this out because error shows up in the console
+};
